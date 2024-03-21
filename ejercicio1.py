976d046d9d09b469f7d0c8a61775ccf483a1876c
xissco <fguillen95@gmail.com>
2024-03-20 21:22:44 -0500

    Tarea

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,7 @@
 class Coche:
     def __init__(self, marca, modelo):
-        # Asigna la marca proporcionada al atributo 'marca'
-        # Asigna el modelo proporcionado al atributo 'modelo'
+        self.marca =  marca # Asigna la marca proporcionada al atributo 'marca'
+        self.modelo = modelo # Asigna el modelo proporcionado al atributo 'modelo'
 
     def conducir(self):
         return f"Conduciendo el {self.marca} {self.modelo}"
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,14 +1,14 @@
 class Animal:
     def __init__(self, nombre):
-        # Asigna el nombre proporcionado al atributo 'nombre'
+        self.nombre = nombre # Asigna el nombre proporcionado al atributo 'nombre'
 
     def hacer_sonido(self):
         pass
 
 class Perro(Animal):
     def hacer_sonido(self):
-        # Devuelve el sonido de un perro (ladrido)
+        return '¡Guau!'# Devuelve el sonido de un perro (ladrido)
 
 class Gato(Animal):
     def hacer_sonido(self):
-        # Devuelve el sonido de un gato (maullido)
+        return '¡Miau!' # Devuelve el sonido de un gato (maullido)
